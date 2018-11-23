99a317343b32c385a0ec078df2fb11e57dd45881
phuongnm <phuongnm.it@mytour.vn>
2018-11-23 17:40:15 +0700

    update add product submit api

--- routes/api/products.php
+++ routes/api/products.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::resource('','ProductController');
+Route::post('/', 'ProductController@store')->name('products.store');
+Route::get('/', 'ProductController@index')->name('products.index');
+Route::get('/{id}', 'ProductController@show')->name('products.show');
+Route::match(['put', 'patch'], '/{id}', 'ProductController@update')->name('products.update');
+Route::delete('/{id}', 'ProductController@destroy');
 
 //Route::group(['middleware' => 'auth:api'], function() {
 //    Route::resource('','ProductController');
